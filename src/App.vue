93c14f1845c41f2db034c883051615d7d3ca6242
Denys Milus <nabajiubass2000@gail.com>
2022-10-04 23:19:53 +0300

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
+<template>
+  <div class="wrapper">
+    <TodoForm/>
+    <TodoItem/>
+  </div>
+</template>
+
 <script setup>
+import TodoForm from "./components/TodoForm/TodoForm.vue";
+import TodoItem from "./components/TodoItem/TodoItem.vue";
 
 </script>
 
-<template>
-  
-</template>
-
 <style scoped>
-
+ .wrapper {
+  height: 100vh;
+  margin: 0 auto;
+  max-width: 860px;
+ }
 </style>
---
+++ src/components/TodoForm/TodoForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="todo-form-wrapper">
+    <h2 class="todo-form-title">Todo App on Vue.js</h2>
+    <div class="todo-form-controls">
+      <input 
+        type="text" 
+        placeholder="Your todo is?"
+      />
+      <button 
+        type="button"
+      >Add</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .todo-form-wrapper {
+    border-bottom: 1px solid #808080;
+    text-align: center;
+  }
+
+  .todo-form-title {
+    letter-spacing: 1px;
+    padding: 15px;
+  }
+
+  .todo-form-controls {
+    padding: 25px;
+  }
+
+  .todo-form-controls input {
+    width: 300px;
+    height: auto;
+    padding: 5px;
+    font-size: 18px;
+  }
+
+  .todo-form-controls button {
+    margin-left: 10px;
+    height: auto;
+    width: 75px;
+    font-size: 18px;
+    background-color: #228B22;
+    color: #fff;
+    font-weight: 600;
+    letter-spacing: 1px;
+    padding: 7px 0px 7px;
+    border-radius: 10px;
+    cursor: pointer;
+    border: 1px solid #808080;
+  }
+</style>
